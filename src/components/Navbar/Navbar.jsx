4c2a270557ad266707504216f5dffdb2f56702cc
danialja81 <danialjdev81@yahoo.com>
2022-02-02 14:40:05 +0330

    add CartItem component to project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ import { ShoppingCart } from '@material-ui/icons';
 import useStyles from './styles';
 import ecommerceLogo from '../../assets/ecommerce.png';
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles();
 
   return (
@@ -30,7 +30,7 @@ const Navbar = () => {
           <div className={classes.grow} />
           <div className={classes.button}>
             <IconButton aria-label='show cart items' color='inherit'>
-              <Badge badgeContent={2} color='secondary'>
+              <Badge badgeContent={totalItems} color='secondary'>
                 <ShoppingCart />
               </Badge>
             </IconButton>
--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,20 @@
 import Commerce from '@chec/commerce.js';
 
 export const commerce = new Commerce(process.env.REACT_APP_COMMERCE_API_KEY);
+
+// export const fetchProducts = async () => {
+//   const { data } = await commerce.products.list();
+//   console.log(data);
+
+//   return Promise.resolve(data);
+// };
+
+// export const fetchCart = async () => {
+//   const response = await commerce.cart.retrieve();
+//   return Promise.resolve(response);
+// };
+
+// export const handleAddToCart = async (productId, quantity) => {
+//   const item = await commerce.cart.add(productId, quantity);
+//   return Promise.resolve(item.cart);
+// };
